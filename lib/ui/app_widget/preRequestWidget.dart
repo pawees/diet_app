34ba53052b7209248fa1b73cef53784f79086881
Симонов Павел Алексеевич <simonovpa@lite.grp>
2022-07-06 17:13:48 +0300

    первичная верстка странички

--- lib/ui/app_widget/preRequestWidget.dart
+++ lib/ui/app_widget/preRequestWidget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_app_training/ui/app_widget/bloc/app_bloc.dart';
+import 'package:game_app_training/ui/app_widget/orderCreateWidget.dart';
+import 'package:game_app_training/ui/theme/styles.dart';
 
 class PreRequestWidget extends StatelessWidget {
   const PreRequestWidget({Key? key}) : super(key: key);
@@ -14,12 +16,54 @@ class PreRequestWidget extends StatelessWidget {
       appBloc.add(HaveNewOrderEvent());
 
     }
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(child: const Text('123')),
-        ElevatedButton(onPressed: _onPressed, child: Text('push me'))
-      ],
+    return SafeArea(
+      child: Column(
+        children: [
+                  Row(children: [
+            IconButton(
+                icon: ImageIcon(AssetImage('assets/images/chevron.png')),
+                onPressed: (){}),
+            Transform(
+              transform:  Matrix4.translationValues( MediaQuery.of(context).size.width/5, 0.0, 0.0),
+              child: const Text('Список заявки',
+                  style: TextStyle(
+                    color: Colors.black, fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+            ),    
+                ],),
+            Divider(height: 3,),
+            Text('Дата',style: header1()),
+            // _build_data_widget(call hive order); 
+            //Divider(height: 1,),
+                SizedBox(height: 16,),
+
+                Text('Учреждение',style: header1()),
+                SizedBox(height: 16,),
+                Agency(title: 'ДДЦ Центр здоровья для детей', address:'610027,Кировская область,г.Киров,ул Красноармейская,43'),
+                SizedBox(height: 20,),
+                Divider(height: 1,),
+                Text('Отделения',style: header1()),
+                SizedBox(height: 25,),
+                Container(
+                  height: 350,// FIXME: relative sizes in project
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      // physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                          return Text('abc');//PlacesWidget(data: places[index], selected: index);
+                          },
+                      itemCount: 10), //places.length,),
+                ),  
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [ 
+                ElevatedButton(onPressed: _onPressed, child: Text('push me'))
+              ],
+            ),
+          ),
+        ],
+      ),
     );
     
   }
