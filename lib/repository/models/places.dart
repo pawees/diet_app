593dd144f932943e13b5f50d17153df11685d6d0
Pavel <pashoksim@gmail.com>
2022-07-11 00:22:10 +0300

    c

--- lib/repository/models/places.dart
+++ lib/repository/models/places.dart
@@ -3,24 +3,23 @@ import 'package:game_app_training/repository/models/diets.dart';
 class Places {
   int id;
   String name;
+  bool isFilled = false;
   List<Diets>? diets;
 
-  Places({required this.id, required this.name,this.diets});
+  Places({required this.id, required this.name, this.diets});
 
- static Places empty = Places(
-    id: 0,
-    name: 'unknow');
-  
-   isFilledMenu(){
+  static Places empty = Places(id: 0, name: 'unknow');
+
+  isFilledMenu() {
     List<Diets> res = [];
-    for (final el in diets!){
-      if (el.count != 0 ){
-      res.add(el);
-    }
+    for (final el in diets!) {
+      if (el.count != 0) {
+        res.add(el);
+      }
     }
     return res;
   }
-    // List<Diets> isFilledMenu(){
+  // List<Diets> isFilledMenu(){
   //   List<Diets> res = [];
   //   for (final el in diets!){
 
@@ -31,14 +30,15 @@ class Places {
   // }
   //constructor that convert json to object instance
   factory Places.fromJson(Map<String, dynamic> json) {
-    return Places(id: json['id'] as int,
-                name: json['name'] as String,
-                );
-    }
+    return Places(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+  }
 
   //a method that convert object to json
   Map<String, dynamic> toJson() => {
-    'uid_1c': id,
-    'name': name,
-  };
+        'uid_1c': id,
+        'name': name,
+      };
 }
--- lib/ui/app_widget/menuWidget.dart
+++ lib/ui/app_widget/menuWidget.dart
@@ -9,21 +9,12 @@ import 'package:game_app_training/ui/theme/main_buttons.dart';
 import 'package:game_app_training/repository/models/diets.dart';
 import 'package:game_app_training/repository/models/order.dart';
 
+_countBuilder(data, AppState state) {
+  if (state.status.isSelected) {
+    return Text(data.count.toString());
+  }
+}
 
-
-
-
-
-
-
-
- _countBuilder(data, AppState state) {
-  
-  if (state.status.isSelected){return Text(data.count.toString());}
- 
-    }
-
-  
 class SummaryAndBtnWidget extends StatelessWidget {
   const SummaryAndBtnWidget({Key? key, required this.state}) : super(key: key);
   final AppState? state;
@@ -33,8 +24,7 @@ class SummaryAndBtnWidget extends StatelessWidget {
     final appBloc = BlocProvider.of<AppBloc>(context);
     var place = state!.places![state!.selected_id];
 
-
-    _next_place(){
+    _next_place() {
       //checking for null diets count,if null drop error.
 
       //использовать не лист,а то,что не будет дублироваться MAP<index,Places>
@@ -43,96 +33,101 @@ class SummaryAndBtnWidget extends StatelessWidget {
       int cur = state!.selected_id;
       int next = cur + 1;
       appBloc.add(NextPlaceEvent(next));
-    };
+    }
 
-    _formed_order(){
-      //check list count not null
+    ;
+
+    _formed_order() {
       List<Places> listing = [];
-      List<Places> finalListing = []; 
-      // state!.places!.map((item)=> {if (item.diets != null){listing.add(item)}else{print('bad')}});
-      state!.places!.map((item)=> {if (item.diets != null){listing.add(item)}}).toList();
- 
-        for (final el in listing){
+      List<Places> finalListing = [];
+      state!.places!
+          .map((item) => {
+                if (item.diets != null) {listing.add(item)}
+              })
+          .toList();
+      //можно занести в блок этот кусок кода.
+
+      for (final el in listing) {
         List<Diets> res = el.isFilledMenu();
-        if (res != null){
+        if (res != null) {
           el.diets = res;
           finalListing.add(el);
         }
-      };
+      }
+      ;
 
       //перебрать листинг,если там одни нули то выкинуть снэк бар с ошибкой.FIXME:
 
-      Order order = Order(id:'130-re3-2', places: finalListing );
+      Order order = Order(id: '130-re3-2', places: finalListing);
       appBloc.add(FormOrderEvent(order));
     }
-    
+
     if (state!.places!.length - 1 == state!.selected_id) {
       return Column(
-      children: [
-        Row(
-          children: [
-            Text('Итого Рацион'),
-            SizedBox(width: 50,), 
-            Text('none') //это значение пока под вопросом.Непонятно что это.
+        children: [
+          Row(
+            children: [
+              Text('Итого Рацион'),
+              SizedBox(
+                width: 50,
+              ),
+              Text('none') //это значение пока под вопросом.Непонятно что это.
             ],
-            ),
-        Container(
-              height: 52,
-              width: 152,
-              child: formedBtn(context, _formed_order) )
-            ],
-            );
-
-    }else{
+          ),
+          Container(
+              height: 52, width: 152, child: formedBtn(context, _formed_order))
+        ],
+      );
+    } else {
       return Column(
-      children: [
-        Row(
-          children: [
-            Text('Итого Рацион'),
-            SizedBox(width: 50,), 
-            Text('none') //это значение пока под вопросом.Непонятно что это.
+        children: [
+          Row(
+            children: [
+              Text('Итого Рацион'),
+              SizedBox(
+                width: 50,
+              ),
+              Text('none') //это значение пока под вопросом.Непонятно что это.
             ],
-            ),
-        Container(
-              height: 52,
-              width: 152,
-              child: mainBtn(context, _next_place) 
-              )
-            ],
-            );
-            }
-    
+          ),
+          Container(
+              height: 52, width: 152, child: mainBtn(context, _next_place))
+        ],
+      );
+    }
   }
 }
 
-
 class _MenuWidgetRow extends StatelessWidget {
-   _MenuWidgetRow({
-    Key? key,
-    required this.state,
-    required this.data,
-    required this.selected}) : super(key: key);
+  _MenuWidgetRow(
+      {Key? key,
+      required this.state,
+      required this.data,
+      required this.selected})
+      : super(key: key);
   final Diets data;
   final int selected;
   var state;
-  
-   @override
+
+  @override
   Widget build(BuildContext context) {
     final appBloc = BlocProvider.of<AppBloc>(context);
 
-    _inc(){
+    _inc() {
       //взять стэйт,селект проверить на существование(создать) прибавить
       appBloc.add(ChangeCountEvent(selected));
-      data.count+=1;
+      data.count += 1;
+    }
 
-    };
-    _decr(){
+    ;
+    _decr() {
       //убавить
       appBloc.add(ChangeCountEvent(selected));
       if (data.count == 0) return;
-      data.count-=1;
+      data.count -= 1;
+    }
 
-    };
+    ;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
       child: Column(
@@ -140,72 +135,84 @@ class _MenuWidgetRow extends StatelessWidget {
           Row(
             mainAxisSize: MainAxisSize.max,
             children: [
-              Icon(Icons.info,color: Colors.blue,),
+              Icon(
+                Icons.info,
+                color: Colors.blue,
+              ),
               SizedBox(width: 15),
-              Expanded(child: Text(data.name, style: header2(),)),
-              IconButton(icon: Icon(Icons.plus_one), onPressed: _inc,),
+              Expanded(
+                  child: Text(
+                data.name,
+                style: header2(),
+              )),
+              IconButton(
+                icon: Icon(Icons.plus_one),
+                onPressed: _inc,
+              ),
               // _countBuilder(data: data),
-              _countBuilder(data,state),
+              _countBuilder(data, state),
 
-              IconButton(icon: Icon(Icons.exposure_minus_1_outlined), onPressed: _decr,),
-
+              IconButton(
+                icon: Icon(Icons.exposure_minus_1_outlined),
+                onPressed: _decr,
+              ),
             ],
           ),
-          SizedBox(height: 28,),
-          Divider(height: 1,),
+          SizedBox(
+            height: 28,
+          ),
+          Divider(
+            height: 1,
+          ),
         ],
       ),
     );
   }
 }
 
-
 class MenuChoiseWidget extends StatelessWidget {
   const MenuChoiseWidget({Key? key}) : super(key: key);
 
- @override
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AppBloc,AppState>(
-      builder: (context,state) {
-        var place = state.places![state.selected_id];
-        if (place.diets == null){
+    return BlocBuilder<AppBloc, AppState>(builder: (context, state) {
+      var place = state.places![state.selected_id];
+      if (place.diets == null) {
+        List<Diets> menuNames = Diets.fetchAll();
+        place.diets = menuNames;
+        // state.order!.places!.add(Places(id: index, name: place.name, diets: menuNames));
 
-                          List<Diets> menuNames = Diets.fetchAll();
-                          place.diets = menuNames;
-                          // state.order!.places!.add(Places(id: index, name: place.name, diets: menuNames));
+      }
 
-                         }
-        
-        return  Padding(
-            padding: const EdgeInsets.fromLTRB(17, 5, 30, 5),
-            child: Column(
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(17, 5, 30, 5),
+        child: Column(
+          children: [
+            HeaderWidget(title: place.name),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                HeaderWidget(title: place.name),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Рацион'),
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                   shrinkWrap: true,
-                   itemBuilder: (context, index) {
-                        //проверить стэйт, если нет то заинитить.
-                         if(index == Diets.fetchAll().length){
-                           return SummaryAndBtnWidget(state: state);
-                         }
-                         return _MenuWidgetRow(data: place.diets![index], selected: index, state: state);
-
-                         },
-                   itemCount: Diets.fetchAll().length + 1
-                   ),
-                ),
+                const Text('Рацион'),
               ],
             ),
-          );
-      }
-    );
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    //проверить стэйт, если нет то заинитить.
+                    if (index == Diets.fetchAll().length) {
+                      return SummaryAndBtnWidget(state: state);
+                    }
+                    return _MenuWidgetRow(
+                        data: place.diets![index],
+                        selected: index,
+                        state: state);
+                  },
+                  itemCount: Diets.fetchAll().length + 1),
+            ),
+          ],
+        ),
+      );
+    });
   }
-
 }
